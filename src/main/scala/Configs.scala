acc8d93ee78b19ec929ba55f95b040411d808bde
Lucas Sanchez <lucaspnm@gmail.com>
2025-10-25 04:04:29 +0000

    Modified Configs.scala

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -1,13 +1,14 @@
+package chipyard
+
 import freechips.rocketchip.config._
 import freechips.rocketchip.subsystem._
+import freechips.rocketchip.diplomacy._
+import reverbrocc._
 
 class WithReverbRoCC extends Config((site, here, up) => {
-  case BuildRoCC => up(BuildRoCC, site) ++ Seq(
-    (p: Parameters) => {
-      val accel = LazyModule(new ReverbRoCC(OpcodeSet.custom0)(p))
-      accel
-    }
-  )
+  case BuildRoCC => up(BuildRoCC, site) ++ Seq( { p: Parameters =>
+    LazyModule(new ReverbRoCC(OpcodeSet.custom0)(p))
+  })
 })
 
 class RocketWithReverbRoCCConfig extends Config(
